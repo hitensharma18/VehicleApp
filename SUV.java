59666b5f4a490e71bd9f6e0c8f69288902bf580a
Hiten Sharma <hitensharma321@gmail.com>
2025-04-27 20:54:03 -0400

    Initial commit

--- SUV.java
+++ SUV.java
@@ -13,17 +13,23 @@ public class SUV extends Car{
         if(newSpeed == 0) {
             stop();
             changeGear( 1);
-        }else if(newSpeed > 0 && newSpeed <= 10) {
+        } 
+        else if(newSpeed > 0 && newSpeed <= 10) {
             changeGear(1);
-        }else if(newSpeed > 10 && newSpeed <= 20) {
+        }
+         else if(newSpeed > 10 && newSpeed <= 20) {
             changeGear(2);
-        }else if(newSpeed > 20 && newSpeed <= 300) {
+        } 
+        else if(newSpeed > 20 && newSpeed <= 300) {
             changeGear(3);
-        }else if(newSpeed > 30 && newSpeed <= 50) {
+        } 
+        else if(newSpeed > 30 && newSpeed <= 50) {
             changeGear(4);
-        }else if(newSpeed > 50 && newSpeed <= 700) {
+        } 
+        else if(newSpeed > 50 && newSpeed <= 70) {
             changeGear(5);
-        } else {
+        } 
+        else {
             changeGear(6);
         }
         if(newSpeed > 0 ) {
